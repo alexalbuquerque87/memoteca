8ef6d537e963dacef3bb13231227359f229043ef
Alex Albuquerque <alexalbuquerqueti@gmail.com>
2023-07-11 22:35:38 -0300

    Exibindo mensagens de erro para conteudo e autoria

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -34,7 +34,9 @@ export class CriarPensamentoComponent implements OnInit {
   }
 
   criarPensamento() {
-    console.log(this.formulario.status);
+    console.log(this.formulario.get('conteudo')?.errors);
+    console.log(this.formulario);
+    console.log(this.formulario.get('autoria')?.errors);
     if(this.formulario.valid){
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento']);
